7d3d1e38ee8b1a51322eaecbdeb252874f5379cd
nhatthanhdev <phamnhatthanh2212@gmail.com>
2024-08-22 00:11:57 +0700

    :::::(((

--- fe-music/music-fe/src/config/firebase.js
+++ fe-music/music-fe/src/config/firebase.js
@@ -6,12 +6,6 @@ import firebase from "firebase/compat/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzOLdETasav8V6zlTEj1QfjRreqOpFYaA",
-  authDomain: "music-web-8e2de.firebaseapp.com",
-  projectId: "music-web-8e2de",
-  storageBucket: "music-web-8e2de.appspot.com",
-  messagingSenderId: "479657678470",
-  appId: "1:479657678470:web:bdef97e91b975d1a06e307"
 };
 firebase.initializeApp(firebaseConfig);
 const auth= firebase.auth
